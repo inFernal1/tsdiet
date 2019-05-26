3d66465f2d18cc4a19872b80591158c0d3f8ddf3
inFernal1 <il-12345@yandex.ru>
2019-05-26 21:47:43 +0300

    error change

--- server/api/get-count-recipes.js
+++ server/api/get-count-recipes.js
@@ -1,10 +1,23 @@
-import { Router } from 'express'
+/*import { Router } from 'express'
 import Recipe from '../models/recipe'
 
 const router = Router();
 
 router.get('/get-count-recipes', (req,res,next) => {
    Recipe.countDocuments({}, (err, count) => { 
+    if(err) return console.log(err);
+    res.json(count);
+})
+})
+export default router
+*/
+import { Router } from 'express'
+import Recipe from '../models/recipe'
+
+const router = Router();
+
+router.get('/get-count-recipes', (req,res,next) => {
+   Recipe.count({}, (err, count) => { 
     if(err) return console.log(err);
     res.json(count);
 })
