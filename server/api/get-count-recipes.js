e6ede5208001a7b3b07f1449149df1d4fdc08d5e
inFernal1 <il-12345@yandex.ru>
2019-05-29 20:11:07 +0300

    nothing

--- server/api/get-count-recipes.js
+++ server/api/get-count-recipes.js
@@ -1,23 +1,10 @@
-/*import { Router } from 'express'
-import Recipe from '../models/recipe'
-
-const router = Router();
-
-router.get('/get-count-recipes', (req,res,next) => {
-   Recipe.countDocuments({}, (err, count) => { 
-    if(err) return console.log(err);
-    res.json(count);
-})
-})
-export default router
-*/
 import { Router } from 'express'
 import Recipe from '../models/recipe'
 
 const router = Router();
 
 router.get('/get-count-recipes', (req,res,next) => {
-   Recipe.count({}, (err, count) => { 
+   Recipe.countDocuments({}, (err, count) => { 
     if(err) return console.log(err);
     res.json(count);
 })
